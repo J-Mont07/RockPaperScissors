bdeacccfe12b24e697e61cd1249d4accc2a8dcad
Juan Montoya <72569343+J-Mont07@users.noreply.github.com>
2021-06-22 15:13:38 -0500

    Update computer.py

--- computer.py
+++ computer.py
@@ -4,4 +4,4 @@ import random
 comChoices = ["rock", "paper","scissors"]
 waitForCom = "Waiting for computer to choose weapon"
 choice = random.choice(comChoices)
-#print(choice)
+#print(choice) ---> checks for comuter choice
